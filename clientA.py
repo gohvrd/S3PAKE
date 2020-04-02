ae40b0e78114011bc35ddb651360d5f1956de672
gohvrd <artemkuz97@yandex.ru>
2020-04-03 00:25:15 +0300

    Добавлен класс для установки настроек

--- clientA.py
+++ clientA.py
@@ -2,9 +2,18 @@ from twisted.internet import reactor, protocol
 
 from random import seed, randint
 from struct import pack, unpack
+import xml.etree.ElementTree as xml
 import public
 
-pwA = 2
+settings = {
+    "pw": None,
+    "id": None,
+    "q": None,
+    "g": None,
+    "M": None,
+    "N": None,
+}
+
 SK = None
 
 g_power_yz = None
@@ -17,7 +26,7 @@ def connectionInitializationMessage():
     x = randint(1, public.q - 1)
     print('A action [*]: choosing random number x from Zp')
     print('A paramteters [*]: x = ', x)
-    X = public.g ** x * public.M ** pwA
+    X = public.g ** x * public.M ** pw
     print('A action [*]: calculating X')
     print('A paramteters [*]: X = ', X)
 
@@ -35,7 +44,7 @@ def responseMessageHandler(message):
     print('A action [*]: receiving S_Y||alpha from B')
     print('S paramteters (received) [*]: S_Y = ', S_Y)
     print('B paramteters (received) [*]: alpha = ', alpha, '\n')
-    g_power_yz = int(S_Y / (public.G((public.A_identifier, public.S_identifier, public.g ** x)) ** pwA))
+    g_power_yz = int(S_Y / (public.G((public.A_identifier, public.S_identifier, public.g ** x)) ** pw))
     print('A calculates [*]: g^(yz) = ', g_power_yz)
 
     test_alpha = int(public.G((public.A_identifier, public.B_identifier, g_power_yz ** x)))
@@ -86,7 +95,62 @@ class PAKEFactory(protocol.ClientFactory):
         reactor.stop()
 
 
+class ClientSettingsManager():
+    def __init__(self):
+        self.filename = 'client_settings.xml'
+
+    def setSettings(self, inSettings: dict):
+        global settings
+
+        if type(inSettings) is not 'dict':
+            print("Передавать настройки можно только в словаре")
+            return
+
+        for key in settings.keys():
+            newVal = inSettings.get(key)
+            
+            if newVal is not None:
+                settings[key] = newVal
+
+    def createSettingsFile(self):
+        root = xml.Element('settings')
+
+        private = xml.Element('private')
+        public = xml.Element('public')
+
+        root.append(private)
+        root.append(public)
+
+        pw = xml.SubElement(private, 'pw')
+        pw.text = 'test'
+        xml.SubElement(public, 'id')
+        xml.SubElement(public, 'q')
+        xml.SubElement(public, 'g')
+        xml.SubElement(public, 'M')
+
+        tree = xml.ElementTree(root)
+        tree.write(self.filename)        
+
+
+    def getClientSettings(self):
+        global settings
+
+        try:
+            self.clSettings = xml.ElementTree(file=self.filename)
+        except IOError:
+            self.createSettingsFile()
+            self.clSettings = xml.ElementTree(file=self.filename)
+
+        settingsXML = self.clSettings.getroot()
+
+        for settingsType in settingsXML:
+            for parametr in settingsType:
+                settings[parametr.tag] = parametr.text
+
+
 def main():
+    global settings
+
     print('Starting client A [*]: id = ', public.A_identifier)
     print('Starting client A [*]: ip = ', public.A_CLIENT_IP)
     print('Starting client A [*]: connecting')
@@ -94,12 +158,19 @@ def main():
     print('Connection public parameters [*]: g = ', public.g)
     print('Connection public parameters [*]: M = ', public.M)
     print('Connection public parameters [*]: N = ', public.N, '\n')
-    f = PAKEFactory()
-    reactor.connectTCP(public.B_CLIENT_IP, public.B_CLIENT_PORT, f)
+    csm = ClientSettingsManager()
+    print(settings)
+    csm.getClientSettings()
+    print(settings)
+    csm.setSettings([1, 2])
+    print(settings)
+    
+    #f = PAKEFactory()
+    #reactor.connectTCP(public.B_CLIENT_IP, public.B_CLIENT_PORT, f)
 
     #mitm
     #reactor.connectTCP(public.MITM_IP, public.MITM_AS_B_CLIENT_PORT, f)
-    reactor.run()
+    #reactor.run()
 
 
 if __name__ == '__main__':
